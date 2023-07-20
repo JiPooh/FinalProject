c4fed6e3a7ef8ee47be74359f5ac460d5d1d8085
malik <zito0029@algonquinlive.com>
2023-07-20 18:14:23 -0400

    first commit

--- app/src/main/java/algonquin/cst2335/finalproject/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/finalproject/MainActivity.java
@@ -1,10 +1,12 @@
 package algonquin.cst2335.finalproject;
 
-import androidx.appcompat.app.AppCompatActivity;
+import  androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
