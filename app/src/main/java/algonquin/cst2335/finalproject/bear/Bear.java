32a7d5471d423a182c2653155bfcbd9b2b64867a
Jihoo Park <park0588@algonquinlive.com>
2023-07-20 19:09:25 -0400

    bear implementing for milestone 1

--- app/src/main/java/algonquin/cst2335/finalproject/bear/Bear.java
+++ app/src/main/java/algonquin/cst2335/finalproject/bear/Bear.java
@@ -1,8 +1,15 @@
 package algonquin.cst2335.finalproject.bear;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 import algonquin.cst2335.finalproject.R;
 
@@ -12,5 +19,28 @@ public class Bear extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bear);
+        findViewById(R.id.bearGen).setOnClickListener(clk->{
+            Toast.makeText(this, "Bear has been generated!", Toast.LENGTH_SHORT).show();
+        });
+        findViewById(R.id.bearSave).setOnClickListener(clk->{
+            AlertDialog.Builder builder = new AlertDialog.Builder(Bear.this);
+            builder.setMessage("Do you wish to save this image?")
+                    .setTitle("Question")
+                    .setPositiveButton("Yes", (dialog, cl) -> {
+                        //sharedPref save to phone
+                        });
+                        Snackbar.make(findViewById(R.id.bearIMGRV), "You have saved this image, are you sure?", Snackbar.LENGTH_LONG)
+                                .setAction("UNDO", clck -> {
+                                    //remove from phone
+                                })
+                                .show();
+                    })
+                    .setNegativeButton("No", (dialog, cl) -> {
+                    })
+                    .create().show();
+        });
+        //sharedpref
+        //toast somewhere
+
     }
 }
