8aa358517096306f713710c484402911a279dc5c
Taha Nofal <nofa0008@algonquinlive.com>
2023-07-20 19:46:35 -0400

    Added toast,alert, and snackbar to main activity

--- app/src/main/java/algonquin/cst2335/finalproject/flightTracker.java
+++ app/src/main/java/algonquin/cst2335/finalproject/flightTracker.java
@@ -8,17 +8,30 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 
 public class flightTracker extends AppCompatActivity {
     private Button toastButton;
+    private static final String PREF_NAME = "MyPreferences";
+    private static final String KEY_USER_NAME = "UserName";
 
+    private EditText editTextUserName;
+    private SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flight_tracker);
+
+        editTextUserName = findViewById(R.id.airport_option);
+        sharedPreferences = getSharedPreferences(PREF_NAME, MODE_PRIVATE);
+
+        // Load the user's name from Shared Preferences (if available)
+        String savedUserName = sharedPreferences.getString(KEY_USER_NAME, "");
+        editTextUserName.setText(savedUserName);
+
         toastButton = findViewById(R.id.airport_option_button);
         toastButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,17 +39,19 @@ public class flightTracker extends AppCompatActivity {
                 showToast();
                 showAlertDialog();
                 showSnackbar(v);
-
-//        SharedPreferences prefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
-//        prefs.getString("VariableName", String defaultValue);
-//        String airportCode = prefs.getString("Airport Code", "");
-//        SharedPreferences.Editor editor = prefs.edit();
-//        editor.putString(String airportCode, String value);
-//        editor.apply();
             }
         });
     }
+    @Override
+    protected void onPause() {
+        super.onPause();
 
+        // Save the user's name to Shared Preferences when the activity is paused
+        String userName = editTextUserName.getText().toString();
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(KEY_USER_NAME, userName);
+        editor.apply();
+    }
     private void showToast() {
         Toast.makeText(this, "This is a Toast message!", Toast.LENGTH_SHORT).show();
     }
