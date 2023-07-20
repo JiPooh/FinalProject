47bfd99f73da0026ce655349c04e4f107ffb8406
Taha Nofal <nofa0008@algonquinlive.com>
2023-07-20 19:08:19 -0400

    Added flightTracker activity id to main page and created toast,alert, and a snackbar

--- app/src/main/java/algonquin/cst2335/finalproject/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/finalproject/MainActivity.java
@@ -2,6 +2,7 @@ package algonquin.cst2335.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 
@@ -12,7 +13,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViewById(R.id.buttFlight).setOnClickListener(clk->{
-
+            Intent intent = new Intent(MainActivity.this, flightTracker.class);
+            startActivity(intent);
         });
         findViewById(R.id.buttTriv).setOnClickListener(clk->{
 
--- app/src/main/java/algonquin/cst2335/finalproject/flightTracker.java
+++ app/src/main/java/algonquin/cst2335/finalproject/flightTracker.java
@@ -1,14 +1,55 @@
 package algonquin.cst2335.finalproject;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class flightTracker extends AppCompatActivity {
+    private Button toastButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flight_tracker);
+        toastButton = findViewById(R.id.airport_option_button);
+        toastButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast();
+                showAlertDialog();
+                showSnackbar(v);
+
+//        SharedPreferences prefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
+//        prefs.getString("VariableName", String defaultValue);
+//        String airportCode = prefs.getString("Airport Code", "");
+//        SharedPreferences.Editor editor = prefs.edit();
+//        editor.putString(String airportCode, String value);
+//        editor.apply();
+            }
+        });
+    }
+
+    private void showToast() {
+        Toast.makeText(this, "This is a Toast message!", Toast.LENGTH_SHORT).show();
+    }
+
+    private void showAlertDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Alert Dialog")
+                .setMessage("This is an AlertDialog!")
+                .setPositiveButton("OK", null)
+                .show();
+    }
+
+    private void showSnackbar(View view) {
+        Snackbar.make(view, "This is a Snackbar!", Snackbar.LENGTH_SHORT).show();
     }
 }
