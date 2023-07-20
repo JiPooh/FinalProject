f2e36a8f50c2ca3ed68302ce95502aaf8460997e
Jihoo Park <park0588@algonquinlive.com>
2023-07-20 18:07:22 -0400

    Initial commit for bear portion

--- app/src/main/java/algonquin/cst2335/finalproject/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/finalproject/MainActivity.java
@@ -4,11 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.buttFlight).setOnClickListener(clk->{
+
+        });
+        findViewById(R.id.buttTriv).setOnClickListener(clk->{
+
+        });
+        findViewById(R.id.buttCurr).setOnClickListener(clk->{
+
+        });
+        findViewById(R.id.buttBear).setOnClickListener(clk->{
+
+        });
     }
 }
