3ddbd210ef8631030015ab9a60da2986ced398d8
Taha Nofal <nofa0008@algonquinlive.com>
2023-07-20 18:42:33 -0400

    Added buttons and a edittext

---
+++ app/src/main/java/algonquin/cst2335/finalproject/flightTracker.java
@@ -0,0 +1,14 @@
+package algonquin.cst2335.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class flightTracker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flight_tracker);
+    }
+}
