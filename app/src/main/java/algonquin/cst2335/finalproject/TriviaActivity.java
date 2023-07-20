156d2c50667e8b03d87896c30d2435db8c407153
malik <zito0029@algonquinlive.com>
2023-07-20 18:41:51 -0400

    first commit

---
+++ app/src/main/java/algonquin/cst2335/finalproject/TriviaActivity.java
@@ -0,0 +1,16 @@
+package algonquin.cst2335.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TriviaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trivia);
+
+
+    }
+}
