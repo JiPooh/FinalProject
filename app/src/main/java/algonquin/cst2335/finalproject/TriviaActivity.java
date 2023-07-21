3d474112a7a2609d941a91db186adaceae4a6d93
malik <zito0029@algonquinlive.com>
2023-07-20 20:55:09 -0400

    Gui Commit

--- app/src/main/java/algonquin/cst2335/finalproject/TriviaActivity.java
+++ app/src/main/java/algonquin/cst2335/finalproject/TriviaActivity.java
@@ -6,77 +6,66 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 
 public class TriviaActivity extends AppCompatActivity {
 
+    private SharedPreferences sharedPreferences;
 
 
 
 
 
-    AlertDialog.Builder builder = new AlertDialog.Builder( TriviaActivity.this);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_trivia);
+
+
         Button test = findViewById(R.id.testButton) ;
-        RecyclerView triviaList = findViewById(R.id.triviaList);
+       RecyclerView triviaList = findViewById(R.id.triviaList);
+
         Toast.makeText(this,"Welecome to trivia game", Toast.LENGTH_LONG).show();
 
-        findViewById(R.id.backButton).setOnClickListener(clk->{
-            Intent MainIntent = new Intent(TriviaActivity.this , MainActivity.class);
-
-            startActivity(MainIntent) ;
-
-        });
-        findViewById(R.id.randomButton).setOnClickListener(clk->{
-
-        });
-
-
-       test.setOnClickListener(clk -> {
-            builder.setMessage("Do you want to choose this topic?")
-                    .setTitle("Topic")
-                    .setNegativeButton("No", (dialog, which) -> {})
-                    .setPositiveButton("Yes", (dialog, which) -> {
-                        View rootView = findViewById(R.id.testView);
-                        Snackbar.make(rootView , "You are playing the sport topic", Snackbar.LENGTH_LONG)
-                                .setAction("Undo", clk2 -> {
-
-                                }).show();
-                    }).create().show();
-        });
-
-//        triviaList.setAdapter(new RecyclerView.Adapter<MyRowHolder>() {
-//            @NonNull
-//            @Override
-//            public MyRowHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-//                return null;
-//            }
+//        findViewById(R.id.backButton).setOnClickListener(clk->{
+//            Intent MainIntent = new Intent(TriviaActivity.this , MainActivity.class);
 //
-//            @Override
-//            public void onBindViewHolder(@NonNull MyRowHolder holder, int position) {
+//            startActivity(MainIntent) ;
 //
-//            }
-//
-//            @Override
-//            public int getItemCount() {
-//                return 0;
-//            }
 //        });
+
+        test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                showAlertDialog();
+                showSnackbar(v);
+            }
+        });
+
 
 
 
 
     }
+    private void showAlertDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Alert Dialog")
+                .setMessage("This is an AlertDialog!")
+                .setPositiveButton("OK", null)
+                .show();
+    }
 
-
+    private void showSnackbar(View view) {
+        Snackbar.make(view, "This is a Snackbar!", Snackbar.LENGTH_SHORT).show();
+    }
 }
