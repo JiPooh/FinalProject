80119efa673b8b7afc1c861dd3991ffa07562908
malik <zito0029@algonquinlive.com>
2023-08-08 03:38:03 -0400

    Pre Final Commit

--- app/src/main/java/algonquin/cst2335/finalproject/TriviaActivity.java
+++ app/src/main/java/algonquin/cst2335/finalproject/TriviaActivity.java
@@ -7,11 +7,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -26,47 +28,61 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
 
 import algonquin.cst2335.finalproject.databinding.ActivityTriviaBinding;
 
 public class TriviaActivity extends AppCompatActivity {
 
-    private SharedPreferences sharedPreferences;
+
+
+    SharedPreferences  prefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
+
+
 
 
     ActivityTriviaBinding binding;
 
 
-    String category;
+    String selectedCategory;
     RequestQueue queue = null;
+String defaultValue = "" ;
 
-
-
+    String[] categoryIDs ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         queue = Volley.newRequestQueue(this);
         binding = ActivityTriviaBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        SharedPreferences  prefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
+
 
         RecyclerView triviaList = binding.triviaList;
-
+        categoryIDs = getResources().getStringArray(R.array.category_ids);
         Toast.makeText(this, "Welcome to the trivia game", Toast.LENGTH_LONG).show();
 
+        setupSpinner();
         binding.randomButton.setOnClickListener(click -> {
             String questionNumber = binding.questionText.getText().toString();
             int qNumber = Integer.parseInt(questionNumber);
-            category = binding.categoryText.getText().toString();
 
-            String url = buildTriviaURL(qNumber, category);
+            int selectedPosition = binding.categorySpinner.getSelectedItemPosition(); // get the position
+
+            selectedCategory = categoryIDs[selectedPosition];
+
+            String url = buildTriviaURL(qNumber, selectedCategory);
+
+
             fetchTriviaQuestions(url);
         });
 
         triviaList.setLayoutManager(new LinearLayoutManager(this));
     }
 
-    private String buildTriviaURL(int numQuestions, String category) {
-        return "https://opentdb.com/api.php?amount=" + numQuestions + "&category=" + category + "&type=multiple";
+    private String buildTriviaURL(int numQuestions, String selectedCategory) {
+        return "https://opentdb.com/api.php?amount=" + numQuestions + "&category=" + selectedCategory + "&type=multiple";
     }
 
     private void fetchTriviaQuestions(String url) {
@@ -88,6 +104,15 @@ public class TriviaActivity extends AppCompatActivity {
 
         queue.add(request);
     }
+
+    private void setupSpinner() {
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.category_names, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        binding.categorySpinner.setAdapter(adapter);
+    }
+
+
 
     private void showAlertDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
--- app/src/main/java/algonquin/cst2335/finalproject/TriviaRecyclerViewAdapter.java
+++ app/src/main/java/algonquin/cst2335/finalproject/TriviaRecyclerViewAdapter.java
@@ -5,6 +5,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,7 +28,7 @@ public class TriviaRecyclerViewAdapter extends RecyclerView.Adapter<TriviaRecycl
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_trivia, parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_questions, parent, false);
         return new ViewHolder(view);
     }
 
@@ -37,17 +39,30 @@ public class TriviaRecyclerViewAdapter extends RecyclerView.Adapter<TriviaRecycl
             String question = questionObj.getString("question");
             String correctAnswer = questionObj.getString("correct_answer");
 
+            // Assuming you have a "incorrect_answers" JSONArray in your JSON
+            JSONArray incorrectAnswers = questionObj.getJSONArray("incorrect_answers");
+
+            // Set the question to the TextView
             holder.questionTextView.setText(question);
 
+            // Set the choices to the RadioButtons
+            holder.choice1.setText(incorrectAnswers.getString(0));
+            holder.choice2.setText(incorrectAnswers.getString(1));
+            holder.choice3.setText(incorrectAnswers.getString(2));
+            holder.choice4.setText(correctAnswer); // This is a simple way, you might want to randomize the placement
+
             holder.submitButton.setOnClickListener(v -> {
-                String userAnswer = holder.answerEditText.getText().toString().trim();
+                int selectedId = holder.radioGroup.getCheckedRadioButtonId();
+
+                RadioButton selectedRadioButton = holder.itemView.findViewById(selectedId);
+                String userAnswer = selectedRadioButton.getText().toString();
+
                 if (userAnswer.equalsIgnoreCase(correctAnswer)) {
                     Toast.makeText(v.getContext(), "Correct!", Toast.LENGTH_SHORT).show();
                 } else {
                     Toast.makeText(v.getContext(), "Wrong answer. Try again.", Toast.LENGTH_SHORT).show();
                 }
             });
-
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -60,14 +75,19 @@ public class TriviaRecyclerViewAdapter extends RecyclerView.Adapter<TriviaRecycl
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
         TextView questionTextView;
-        EditText answerEditText;
+        RadioGroup radioGroup;
+        RadioButton choice1, choice2, choice3, choice4;
         Button submitButton;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            questionTextView = itemView.findViewById(R.id.questionText);
-            answerEditText = itemView.findViewById(R.id.answerText);
-            submitButton = itemView.findViewById(R.id.testButton);
+            questionTextView = itemView.findViewById(R.id.questionTextView);
+            radioGroup = itemView.findViewById(R.id.radioGroup);
+            choice1 = itemView.findViewById(R.id.choice1);
+            choice2 = itemView.findViewById(R.id.choice2);
+            choice3 = itemView.findViewById(R.id.choice3);
+            choice4 = itemView.findViewById(R.id.choice4);
+            submitButton = itemView.findViewById(R.id.submitButton);
         }
     }
 }
